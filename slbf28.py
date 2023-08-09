4fd80797f11a62924d470ce0b644f0134e4d6ce2
cxy <2573087386@qq.com>
2023-08-09 16:42:18 +0800

    test

--- slbf28.py
+++ slbf28.py
@@ -9,9 +9,71 @@ import os
 
 import openpyxl
 
-# 指定文件夹路径
+# tablename 字典
+table_name_dict = {1: "gh_fixed_wages",
+                   2: "gh_fixed_welfare",
+                   3: "gh_five_risks",
+                   4: "gh_accumulation_fund",
+                   5: "gh_annuity",
+                   6: "gh_zhe_jiu",
+                   7: "gh_fixed_car_insurance",
+                   8: "gh_fix_cailiao",
+                   9: "gh_traffic_expense",
+                   10: "gh_item_laowu",
+                   11: "gh_item_gongnong",
+                   12: "gh_fixed_water",
+                   13: "gh_repair",
+                   14: "gh_item_waigu",
+                   15: "gh_fixed_taxes",
+                   16: "",
+                   17: "gh_fixed_travel_expense",
+                   18: "gh_fixed_office_allowance",
+                   19: "gh_fixed_print",
+                   20: "gh_computer",
+                   21: "gh_decorate",
+                   22: "gh_fixed_communication_expense",
+                   23: "gh_fixed_training_expense",
+                   24: "gh_fixed_test_fee",
+                   25: "gh_fixed_labour_protection",
+                   26: "gh_fixed_agency_fee",
+                   27: "gh_item_other_expenses",
+                   28: "gh_fix_trade"}
 
-folder_path = './testExcelFile'
+# type_classe_dict 字典
+file_name_dist = {
+    'gongzi': 1,
+    'fuli': 2,
+    'wuxian': 3,
+    'zhufanggongjijin': 4,
+    'nianjin': 5,
+    'zhejiutanxiao': 6,
+    'cheliangbaoxian': 7,
+    'cailiaofei': 8,
+    'jixiefei': 9,
+    'waifulaowu': 10,
+    'laowufei': 11,
+    'shuidianfei': 12,
+    'xiulifei': 13,
+    'linshigongzi': 14,
+    'shuijin': 15,
+    'dizhiyihaopin': 16,
+    'chalvfei': 17,
+    'yewuzhaodaifei': 18,
+    'yinshuafei': 19,
+    'weijihaocai': 20,
+    'zhuangshifei': 21,
+    'tongxunfei': 22,
+    'peixunfei': 23,
+    'jiancefei': 24,
+    'laodongbaohufei': 25,
+    'zhongjiefei': 26,
+    'qitazhichu': 27,
+    'shangmaochengben': 28
+}
+
+# 指定要处理的文件夹路径
+# folder_path = './testExcelFile'
+folder_path = 'D://Projects//gh//slbfNew//28表更改信息//胜利北方公司修改后//建筑公司//测试vba'
 
 # 创建一个新的工作簿用于存储提取的数据
 new_workbook = openpyxl.Workbook()
@@ -24,6 +86,22 @@ print(os.listdir(folder_path))
 for filename in os.listdir(folder_path):
     if filename.endswith('.xlsx'):
         source_filepath = os.path.join(folder_path, filename)
+
+        # 截取_和.之间的文件名
+        index1 = 0
+        index2 = 0
+        j = 0
+        for i in filename:
+            j += 1
+            if i == "_":
+                index1 = j
+            elif i == ".":
+                index2 = j
+            else:
+                continue
+        type_classe_name = filename[index1: index2 - 1]
+        # 获取根据文件名获取类型
+        type_classe_value = file_name_dist[type_classe_name]
 
         # 打开源工作簿
         source_workbook = openpyxl.load_workbook(source_filepath)
@@ -39,18 +117,20 @@ for filename in os.listdir(folder_path):
         for col_index, header in enumerate(header_row):
             if header == "type_classe":
                 type_classe_col = col_index + 1  # 列索引从1开始
-
-        # 遍历处第一行外每一行的第一个单元格确定背景填充色
-        for i in range(2, max_row + 1):
-            # 获取表格 填充色 颜色
+        print(filename)
+        # 遍历除第一行外每一行的第一个单元格确定背景填充色
+        for i in range(2, max_row):
+            print(i)
+            # 获取表格填充色颜色
             color = source_sheet.cell(i, 1).fill.fgColor.rgb
+            print(color)
             # 获取黄色行的id和type_classe
             if color == "FFFFFF00":
-                id_value = source_sheet.cell(i, id_col).value
-                type_classe_value = source_sheet.cell(i, type_classe_col).value
-                new_sheet.append([id_value, type_classe_value])
+                # id 截取第一个字符之后的字符串
+                id_value = source_sheet.cell(i, id_col).value[1:]
+                new_sheet.append([id_value, type_classe_value, table_name_dict[type_classe_value]])
 
         source_workbook.close()
 
-# 保存新的工作簿到新文件
+# 保存新的工作簿到新文件并命名
 new_workbook.save('filtered_data.xlsx')
